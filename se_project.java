28445ab84b775c3411aa9d5fd7b0622bd31148d5
majiddell <48320237+majiddell@users.noreply.github.com>
2020-07-20 02:31:41 +0430

    first

---
+++ se_project.java
@@ -0,0 +1,14 @@
+package sep;
+
+public class se_project {
+
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("first version");
+		}
+		
+	}
+	
+}
